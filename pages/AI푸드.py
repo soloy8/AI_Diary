913719311e4fa1d7fabc9b44215d147dc1b95624
soloy8 <loyalblue17@naver.com>
2021-12-06 02:32:10 +0900

    '

--- pages/AI푸드.py
+++ pages/AI푸드.py
@@ -7,7 +7,8 @@ from webpage.web_food_predict import *
 def write():
     #위 구문 안쓰면 오류임
 
-    st.title('식사 맛있게 하시고 \n 따뜻한 시간 보내세요~')
+    st.title('식사 맛있게 하시고'')
+    st.title('따뜻한 시간 보내세요~')
     input_data = st.text_area("식사 전의 기분을 입력하세요!")
 
     # 여기서 input_data를 감정분석 프로그램에 넘겨주고 그 인수를 받아와야 한다. - 해결!
