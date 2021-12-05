13efaedbe7ea85161f92217d7d909133dd512b54
soloy8 <loyalblue17@naver.com>
2021-12-05 22:49:12 +0900

    '

--- pages/AI푸드.py
+++ pages/AI푸드.py
@@ -13,6 +13,17 @@ import numpy as np
 def write():
     #위 구문 안쓰면 오류임
 
+    input_data = st.text_area("식사 전의 기분을 입력하세요!")
+
+    # 여기서 input_data를 감정분석 프로그램에 넘겨주고 그 인수를 받아와야 한다. - 해결!
+
+    Push_listen_button = st.button("AI 음식 추천")
+    if Push_listen_button:
+        result,link = predict(input_data)
+        st.success(result)
+
+
+    from PIL import Image
     st.image('https://cdn.pixabay.com/photo/2016/08/20/13/06/toppokki-1607479_960_720.jpg', width=700, caption="Image example: Diary")  # 400잡으면 전체 400축소됨.
 
     st.write(
